5596ff1cbbb7efbf119f2dd2bd076f87b4ae4f92
przemked <przemekdobrenko@gmail.com>
2020-04-14 12:49:45 +0200

    Updates to assure compatibility with prev Python versions (removing walrus operator)

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 
 # configuration
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['PROPAGATE_EXCEPTIONS'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'przemek'
 
